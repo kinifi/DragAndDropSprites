0ae21816c22494890b46bacbf6021ba52db9e6c5
kinifi <chrisfigueroa16@gmail.com>
2017-12-08 17:58:45 -0800

    added everything

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+    private bool isMoving = false;
+    private Vector2 mousePosition;
+
+    public GameObject[] m_Placements;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//has the user selected us?
+        if(!isMoving)
+        {
+            return;
+        }
+
+        //get the mouse position in world cooridinates
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //move the sprite with the mouse position
+        this.transform.position = new Vector2(mousePosition.x, mousePosition.y);
+
+	}
+
+
+    private void OnMouseDown()
+    {
+        if(!GameManager.instance.isDragging)
+        {
+            isMoving = true;
+            GameManager.instance.isDragging = true;
+        }
+
+        //Debug.Log("On Mouse Down");
+    }
+
+    private void OnMouseUp()
+    {
+        if (GameManager.instance.isDragging && isMoving == true)
+        {
+            isMoving = false;
+            GameManager.instance.isDragging = false;
+            //check placements
+            bool isPlacedOnPlacement = GameManager.instance.isPlaced(this.gameObject);
+            if(isPlacedOnPlacement == false)
+            {
+                Debug.Log("Placed: " + isPlacedOnPlacement);
+            }
+        }
+    }
+
+    public void OnPlaced(GameObject placement)
+    {
+        Debug.Log("OnPlaced: " + placement.name);
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public bool isDragging = false;
+
+    public GameObject[] m_Placements;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+        {
+
+            //if not, set instance to this
+            instance = this;
+        }
+        else if (instance != this)
+        {
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame() {
+        Debug.Log("Init Game");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool isPlaced(GameObject setDownGameObject)
+    {
+        for (int i = 0; i < m_Placements.Length; i++)
+        {
+            bool overSprite = setDownGameObject.gameObject.GetComponent<SpriteRenderer>().bounds.Contains(m_Placements[i].transform.position);
+            if (overSprite)
+            {
+                //Debug.Log("On Top Of: " + m_Placements[i].name);
+                setDownGameObject.SendMessage("OnPlaced", m_Placements[i], SendMessageOptions.RequireReceiver);
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
